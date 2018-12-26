1297ea0465c8de50c462f2b54ee7ca53da0825f1
AnastasiaEs <anastasia.lukina.es@gmail.com>
2018-12-26 22:44:21 +0100

    Cambio en la rama de test

--- PrimerProyectoR.R
+++ PrimerProyectoR.R
@@ -1,2 +1,7 @@
+#git config --global user.email "you@example.com"
+#git config --global user.name "AnastasiaEs"
+
 c(1,5,3,2) 
 c("barcelona","tarragona","lerida","gerona") 
+
+#Cambio realizado en la rama Test 
